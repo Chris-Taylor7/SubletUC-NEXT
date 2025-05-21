a2b9519f21f70de71131f3dfc9517c13cba6941a
Chris-Taylor7 <chtaylor270@gmail.com>
2025-05-21 13:58:34 -0400

    temp login form stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { AuthenticationForm } from "@/components/Login/page";
+
 export default function HomePage() {
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <h1 className="text-3xl font-bold text-[#e00122] mb-4">Welcome to UC Room Listings</h1>
       <p className="text-lg">Find and list off-campus housing with fellow Bearcats.</p>
+
+          <AuthenticationForm></AuthenticationForm>
+
     </div>
+
   );
 }
 
